924c74137ce99b413c218f934fbe3fe16b8efb14
Romenildo <Romenildoferreira@gmail.com>
2022-07-23 15:04:31 -0300

    add base professor

---
+++ professorConsumer/src/test/java/com/edu/professorConsumer/ProfessorConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.professorConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
